024ab531a3e20702e1ae6adac8165d435e214118
ayushjaipuriyar <ayushjaipuriyar21@gmail.com>
2021-08-29 11:37:46 +0530

    Update

---
+++ React/food/src/store/CartProvider.jsx
@@ -0,0 +1,48 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatesItems = state.items.concat(action.item);
+    const updatesTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatesItems, totalAmount: updatesTotalAmount };
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <div>
+      <CartContext.Provider value={cartContext}>
+        {props.children}
+      </CartContext.Provider>
+    </div>
+  );
+};
+
+export default CartProvider;
