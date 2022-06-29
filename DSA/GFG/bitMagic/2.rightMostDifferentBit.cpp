237797474dcac4cbf930c9ff46ced28b90424bd8
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2022-06-29 12:12:08 +0530

    Removing DSA

--- DSA/GFG/bitMagic/2.rightMostDifferentBit.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-  int x = m ^ n;
-  cout << log2(x & ~(x - 1)) + 1;
-}
--- DSA/cses/numspiral.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  long long t;
-  while (t-- > 0)
-    return 0;
-}
